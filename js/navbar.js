91919a78045def1345d9890fa9fb03d9df837b20
Volodymyr <branets.volodymyr@gmail.com>
2022-08-03 19:41:31 +0300

    Add navbar template and cart modal  for all pages

---
+++ js/navbar.js
@@ -0,0 +1,36 @@
+const head = document.querySelector('#head');
+
+let navbar = `
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark py-3">
+            <div class="container">
+                <a href="index.html" class="navbar-brand text-warning logo"><span class="material-symbols-outlined">
+                        local_mall
+                    </span> SmartStore</a>
+                <!-- Hamburger menu -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu"><span
+                        class="navbar-toggler-icon"></span></button>
+                <!-- Menu List -->
+                <div class="collapse navbar-collapse" id="navmenu">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a href="index.html" class="nav-link">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="products.html" class="nav-link">Some products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="about.html" class="nav-link">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="contacts.html" class="nav-link">Contacts</a>
+                        </li>
+                    </ul>
+                    <a class="nav-item cart text-warning align-content-center" data-bs-toggle="modal"
+                        data-bs-target="#modal-cart"><span
+                            class="material-symbols-outlined">shopping_cart_checkout</span><span
+                            class="badge border border-warning cart-badge"></span></a>
+                </div>
+            </div>
+        </nav>`;
+
+head.innerHTML = navbar;
